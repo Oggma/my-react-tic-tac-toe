2e2eba276150c0fdc392f79e864506598de074fc
LINKER\r.svezhentsev <raman.svezhantsau@gmail.com>
2020-11-24 17:34:37 +0300

    init squares with loops

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,25 +12,17 @@ class Board extends React.Component {
     }
 
     render() {
+        const bordRows = new Array(3);
+        for (var i = 0; i < 9;) {
+            var rowSquares = new Array(3);
+            for (var j = 0; j < 3; i++, j++) {
+                rowSquares.push(this.renderSquare(i));
+            }
+            bordRows.push(<div className="board-row"> {rowSquares} </div>)
+        }
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {bordRows}
             </div>
         );
     }
